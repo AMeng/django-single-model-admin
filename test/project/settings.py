bff823b73e09782ac54744fbeba6ec7a1cffdd03
AMeng <alexbmeng@gmail.com>
2016-09-03 12:00:32 -0600

    Add support for Django 1.10. Drop support for Python 3.2.

--- test/project/settings.py
+++ test/project/settings.py
@@ -27,6 +27,22 @@ MIDDLEWARE_CLASSES = (
 ROOT_URLCONF = 'project.urls'
 WSGI_APPLICATION = 'project.wsgi.application'
 
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
