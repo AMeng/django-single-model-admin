4f1769a040811354a93ce71730e3b8eed5ec6e43
ameng <alexbmeng@gmail.com>
2018-08-01 15:08:31 -0600

    Support Django 2.1

--- setup.py
+++ setup.py
@@ -1,3 +1,4 @@
+import setuptools
 from distutils.core import setup
 
 setup(
@@ -14,6 +15,7 @@ setup(
     ],
     classifiers=[
         "Development Status :: 5 - Production/Stable",
+        "Framework :: Django",
         "Framework :: Django :: 1.4",
         "Framework :: Django :: 1.5",
         "Framework :: Django :: 1.6",
@@ -23,10 +25,16 @@ setup(
         "Framework :: Django :: 1.10",
         "Framework :: Django :: 1.11",
         "Framework :: Django :: 2.0",
+        "Framework :: Django :: 2.1",
         "Intended Audience :: Developers",
         "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
         "Programming Language :: Python :: 2.6",
         "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
